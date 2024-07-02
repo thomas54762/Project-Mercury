5bfd9be1bb472d2903e6e5b18c92f03bf0f8766f
Thomas van Buuren <68168903+thomas54762@users.noreply.github.com>
2024-07-02 06:56:00 +0200

    fix time issues

--- main.py
+++ main.py
@@ -3,72 +3,77 @@ from src.tradingview_screener.query import Column
 from openpyxl import load_workbook
 import os
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+import schedule
+import time
 
 alpha_vantage_key = "6XSD0XKGJCSLNBRR"
 interval = '1min'
 outputsize = 'full'
 file_path = 'stock_data.xlsx'
 
-n_rows, df = (Query()
- .select('name', 'close', 'premarket_close', 'premarket_change', 'premarket_volume')
- .where(
-    Column('premarket_change') > 10,
-    Column('premarket_volume') > 3_000_000,
-    Column('premarket_close') > 0.9,
-    Column('exchange').isin(['NASDAQ', 'NYSE'])
- ).get_scanner_data())
+def run_daily_task():
+   _, df = (Query()
+   .select('name', 'close', 'premarket_close', 'premarket_change', 'premarket_volume')
+   .where(
+      Column('premarket_change') > 10,
+      Column('premarket_volume') > 3_000_000,
+      Column('premarket_close') > 0.9,
+      Column('exchange').isin(['NASDAQ', 'NYSE'])
+   ).get_scanner_data())
 
+   # TO-DO
+   # make a dictionary to store more premarket data.
+   tickers = df["name"].tolist()
 
-# make a dictionary to store more premarket data.
-tickers = df["name"].tolist()
+   try:
+      for ticker in tickers:
+         today = datetime.today()
+         latest_trading_day = today - timedelta(days=1)
 
-for ticker in tickers:
-   today = datetime.today()
-   latest_trading_day = today
+         #while latest_trading_day.weekday() >= 5:
+         #latest_trading_day -= timedelta(days=1)
 
-   while latest_trading_day.weekday() >= 5:
-      latest_trading_day -= timedelta(days=1)
+         latest_trading_day_str = latest_trading_day.strftime('%Y-%m-%d')
+         url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval={interval}&outputsize={outputsize}&apikey={alpha_vantage_key}'
+         response = requests.get(url)
 
-   latest_trading_day_str = latest_trading_day.strftime('%Y-%m-%d')
+         if response.status_code == 200:
+            data = response.json()
+            time_series = data[f'Time Series ({interval})']
+            first_15_bars = []
 
-   url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval={interval}&outputsize={outputsize}&apikey={alpha_vantage_key}'
-   response = requests.get(url)
+            for timestamp, bar_data in time_series.items():
+               if timestamp.startswith(latest_trading_day_str):
+                  timestamp_time = timestamp.split()[1]
+                  if '09:30:00' <= timestamp_time < '09:45:00':
+                     first_15_bars.append(bar_data)
+                     if len(first_15_bars) == 15:
+                        break
 
-   if response.status_code == 200:
-      data = response.json()
-      time_series = data[f'Time Series ({interval})']
-      first_15_bars = []
+            if len(first_15_bars) < 15:
+               print(f"Warning: Less than 15 bars found for {latest_trading_day_str}")
 
-      for timestamp, bar_data in time_series.items():
-         if timestamp.startswith(latest_trading_day_str):
-            timestamp_time = timestamp.split()[1]
-            if '09:30:00' <= timestamp_time < '09:45:00':
-               first_15_bars.append(bar_data)
-               if len(first_15_bars) == 15:
-                  break
+            lastBar = first_15_bars[0]
+            firstBar = first_15_bars[-1]
+            
+            openFirstBar = float(firstBar["1. open"])
+            closeLastBar = float(lastBar["4. close"])
+            
+            percentageGain = ((closeLastBar - openFirstBar) / openFirstBar) * 100
+            totalGain = (100 / 100) * (percentageGain + 100)
+            
+            wb = load_workbook(filename=file_path)
+            sheet = wb.active
+            
+            # TO-DO
+            # can we also get the market cap and avg volume by any chance?
+            new_data = [latest_trading_day_str, ticker, openFirstBar, closeLastBar, percentageGain, totalGain]
+            sheet.append(new_data)
+            wb.save(file_path)
+         else:
+            print(f"Error fetching data. Status code: {response.status_code}")
+   except Exception as e:
+      print(f"{e}")
 
-      if len(first_15_bars) < 15:
-         print(f"Warning: Less than 15 bars found for {latest_trading_day_str}")
-
-      lastBar = first_15_bars[0]
-      firstBar = first_15_bars[-1]
-      
-      openFirstBar = float(firstBar["1. open"])
-      closeLastBar = float(lastBar["4. close"])
-      
-      percentageGain = ((closeLastBar - openFirstBar) / openFirstBar) * 100
-      totalGain = (100 / 100) * (percentageGain + 100)
-      
-      wb = load_workbook(filename=file_path)
-      sheet = wb.active
-      
-      # can we also get the market cap and avg volume by any chance?
-      new_data = [latest_trading_day_str, ticker, openFirstBar, closeLastBar, percentageGain, totalGain]
-      sheet.append(new_data)
-      wb.save(file_path)
-      
-   else:
-      print(f"Error fetching data. Status code: {response.status_code}")
-
-
+run_daily_task()
